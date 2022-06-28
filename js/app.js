cd45b5406d4b57f6f51dfe7c52e786799ae46e3f
Andy Martin <106330492+AndyMartin10@users.noreply.github.com>
2022-06-28 19:37:06 +0200

    subiendo archivo para visualizar correctamente

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function imgSlider(anything) {
+  document.querySelector('.pepsi').src = anything;
+}
+
+function changeBgColor(color) {
+  const sec = document.querySelector('body');
+  sec.style.background = color;
+
+}
